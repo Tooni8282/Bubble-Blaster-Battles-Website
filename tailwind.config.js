b8950e954c2a31c17cc191211b681d8f46a6cf26
Julian Friesen <jubanator3000@gmail.com>
2022-06-23 21:01:27 -0700

    Button gradient animations and title text outlines added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
             xs: "475px",
             ...screens,
         },
+        extend: {
+            backgroundSize: {
+                "size-200": "200% 200%",
+            },
+            backgroundPosition: {
+                "pos-0": "0% 0%",
+                "pos-100": "100% 100%",
+            },
+        },
     },
     future: {
         hoverOnlyWhenSupported: true,
